b82fdd98e1cdec458ae3292635578efe5448bcfb
supertamzer <60706571+supertamzer@users.noreply.github.com>
2020-04-28 22:49:16 +0200

    1+2

--- 1/sketch.js
+++ 1/sketch.js
@@ -1,21 +1,24 @@
 function setup() {
-  let navWidth = document.getElementById("nav").offsetWidth;
-  let canvas = createCanvas(windowWidth - navWidth, windowHeight);
-  canvas.parent('canvas');
-  background(255);
+    let navWidth = document.getElementById("nav").offsetWidth;
+    let canvas = createCanvas(windowWidth - navWidth, windowHeight);
+    canvas.parent('canvas');
+    background(255);
 }
 
 function draw() {
-  let x = 10;
-  fill(0);
-  while (x < width) {
-    ellipse(x, height / 2, 10, 10);
-    x = x + 30;
-  }
+    let r = random(1, 50);
+    noFill();
+    for (let x = 0; x < width; x = x + 30) {
+        for (let y = 0; y < height; y = y + 21) {
+            circle(x, y, 150);
+        }
+    }
 }
 
+
+
 function windowResized() {
-  let navWidth = document.getElementById("nav").offsetWidth;
-  resizeCanvas(windowWidth - navWidth, windowHeight);
-  background(255);
+    let navWidth = document.getElementById("nav").offsetWidth;
+    resizeCanvas(windowWidth - navWidth, windowHeight);
+    background(255);
 }
--- 2/sketch.js
+++ 2/sketch.js
@@ -1,19 +1,21 @@
 function setup() {
-  let navWidth = document.getElementById("nav").offsetWidth;
-  let canvas = createCanvas(windowWidth - navWidth, windowHeight);
-  canvas.parent('canvas');
-  background(255);
+    let navWidth = document.getElementById("nav").offsetWidth;
+    let canvas = createCanvas(windowWidth - navWidth, windowHeight);
+    canvas.parent('canvas');
+    background(255);
 }
 
 function draw() {
-  fill(0);
-  for (let x = 10; x < width; x = x + 30) {
-    ellipse(x, height / 2, 10, 10);
-  }
+    noFill(0);
+    for (let x = 1; x < height; x = x + 30) {
+        for (let y = 1; y < width; y = y + 30) {
+            triangle(y, x, 0, height, width, height);
+        }
+    }
 }
 
 function windowResized() {
-  let navWidth = document.getElementById("nav").offsetWidth;
-  resizeCanvas(windowWidth - navWidth, windowHeight);
-  background(255);
+    let navWidth = document.getElementById("nav").offsetWidth;
+    resizeCanvas(windowWidth - navWidth, windowHeight);
+    background(255);
 }
